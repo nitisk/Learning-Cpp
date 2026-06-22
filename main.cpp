e96d1e44ddce751570ae1c892c95de3ab963f947
Nitish Kumar <nitishk2348@gmail.com>
2026-06-22 09:39:55 +0530

    learning cpp first day

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //preprocessor statement
+
+//first compiler preprocceses all your preprocessor statements
+//iostream is a header file
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+}
